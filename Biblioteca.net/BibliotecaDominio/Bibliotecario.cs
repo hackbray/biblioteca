d6b48827c0619f37e084c2fd6e26ba5a3f412daa
hackbray <hackbray@gmail.com>
2019-07-02 02:53:51 -0500

    Primer commit

---
+++ Biblioteca.net/BibliotecaDominio/Bibliotecario.cs
@@ -0,0 +1,99 @@
+﻿using BibliotecaDominio.IRepositorio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BibliotecaDominio
+{
+    public class Bibliotecario
+    {
+        public const string EL_LIBRO_NO_SE_ENCUENTRA_DISPONIBLE = "El libro no se encuentra disponible";
+        public const string LOS_LIBROS_PALINDROMOS_SOLO_EN_LA_BIBLIOTECA = "Los libros palíndromos solo se pueden utilizar en la biblioteca";
+        public const int DIAS_MAXIMO_DE_ENTREGA = 15;
+        public const int NUMERO_MAXIMO = 30;
+        private IRepositorioLibro libroRepositorio;
+        private IRepositorioPrestamo prestamoRepositorio;
+
+        public Bibliotecario() { }
+
+        public Bibliotecario(IRepositorioLibro libroRepositorio, IRepositorioPrestamo prestamoRepositorio)
+        {
+            this.libroRepositorio = libroRepositorio;
+            this.prestamoRepositorio = prestamoRepositorio;
+        }
+
+        public void Prestar(string isbn, string nombreUsuario)
+        {
+            var libroExistenciaVerificada = libroRepositorio.ObtenerPorIsbn(isbn);
+            if (libroExistenciaVerificada != null)
+            {
+                if (EsPrestado(isbn))
+                {
+                    if (!EsPalindroma(isbn))
+                    {
+                        prestamoRepositorio.Agregar(new Prestamo(DateTime.Now, libroExistenciaVerificada, CalcularFechaEntregaMaxima(isbn), nombreUsuario));
+                    }
+                    else
+                    {
+                        Console.WriteLine(LOS_LIBROS_PALINDROMOS_SOLO_EN_LA_BIBLIOTECA);
+                        throw new Exception(LOS_LIBROS_PALINDROMOS_SOLO_EN_LA_BIBLIOTECA);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(EL_LIBRO_NO_SE_ENCUENTRA_DISPONIBLE);
+                    throw new Exception(EL_LIBRO_NO_SE_ENCUENTRA_DISPONIBLE);
+                }
+            }
+            else
+            {
+                Console.WriteLine(EL_LIBRO_NO_SE_ENCUENTRA_DISPONIBLE);
+                throw new Exception(EL_LIBRO_NO_SE_ENCUENTRA_DISPONIBLE);
+            }
+        }
+
+        public static DateTime CalcularFechaEntregaMaxima(String isbn)
+        {
+            DateTime fechaMaxima = new DateTime();
+            if (EsMayorQue(NUMERO_MAXIMO, isbn))
+            {
+                for (int k = 1; k < DIAS_MAXIMO_DE_ENTREGA; k++)
+                {
+                    while (fechaMaxima.DayOfWeek == DayOfWeek.Sunday) fechaMaxima = fechaMaxima.AddDays(1);
+                    fechaMaxima = fechaMaxima.AddDays(1);
+                }
+            }
+            return fechaMaxima;
+        }
+
+        public static bool EsMayorQue(int maximo, string isbn)
+        {
+            int sumaDeNumeros = 0;
+            bool esMayor = false;
+
+            foreach (char caracter in isbn)
+            {
+                if (char.IsDigit(caracter))
+                    sumaDeNumeros += Convert.ToInt32(caracter.ToString());
+            }
+            esMayor = sumaDeNumeros > maximo ? true : false;
+            return esMayor;
+        }
+
+        public bool EsPrestado(string isbn)
+        {
+            var libroPrestado = prestamoRepositorio.ObtenerLibroPrestadoPorIsbn(isbn);
+            if (libroPrestado != null)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool EsPalindroma(String cadena)
+        {
+            if (cadena.Length < 2) return true;
+            if (cadena[0] == cadena[cadena.Length - 1]) return EsPalindroma(cadena.Substring(1, cadena.Length - 2));
+            return false;
+        }
+    }
+}
---
+++ Biblioteca.net/DominioTest/Integracion/BibliotecarioTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BibliotecaDominio;
+using BibliotecaRepositorio.Contexto;
+using BibliotecaRepositorio.Repositorio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DominioTest.TestDataBuilders;
+using Microsoft.EntityFrameworkCore;
+
+namespace DominioTest.Integracion
+{
+    [TestClass]
+    public class BibliotecarioTest
+    {
+        public const String CRONICA_UNA_MUERTE_ANUNCIADA = "Cronica de una muerte anunciada";
+        private  BibliotecaContexto contexto;
+        private  RepositorioLibroEF repositorioLibro;
+        private RepositorioPrestamoEF repositorioPrestamo;
+
+
+        [TestInitialize]
+        public void setup()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BibliotecaContexto>();
+            contexto = new BibliotecaContexto(optionsBuilder.Options);
+            repositorioLibro  = new RepositorioLibroEF(contexto);
+            repositorioPrestamo = new RepositorioPrestamoEF(contexto, repositorioLibro);
+        }
+
+        [TestMethod]
+        public void PrestarLibroTest()
+        {
+            // Arrange
+            Libro libro = new LibroTestDataBuilder().ConTitulo(CRONICA_UNA_MUERTE_ANUNCIADA).Build();
+            repositorioLibro.Agregar(libro);
+            Bibliotecario bibliotecario = new Bibliotecario(repositorioLibro, repositorioPrestamo);
+
+            // Act
+            bibliotecario.Prestar(libro.Isbn, "Juan");
+
+            // Assert
+            Assert.AreEqual(bibliotecario.EsPrestado(libro.Isbn), true);
+            Assert.IsNotNull(repositorioPrestamo.ObtenerLibroPrestadoPorIsbn(libro.Isbn));
+
+        }
+
+        [TestMethod]
+        public void PrestarLibroNoDisponibleTest()
+        {
+            // Arrange
+            Libro libro = new LibroTestDataBuilder().ConTitulo(CRONICA_UNA_MUERTE_ANUNCIADA).Build();
+            repositorioLibro.Agregar(libro);
+            Bibliotecario bibliotecario = new Bibliotecario(repositorioLibro, repositorioPrestamo);
+
+            // Act
+            bibliotecario.Prestar(libro.Isbn,"Juan");
+            try
+            {
+                bibliotecario.Prestar(libro.Isbn, "Juan");
+                Assert.Fail();
+            }
+            catch (Exception err)
+            {
+                // Assert
+                Assert.AreEqual("El libro no se encuentra disponible", err.Message);
+            }
+        
+        }
+
+        [TestMethod]
+        public void EsPalindromaTest()
+        {
+            string palindromo = "luzazul";
+            string noPalindromo = "prueba";
+           
+            Assert.AreEqual(Bibliotecario.EsPalindroma(palindromo),true);
+            Assert.AreEqual(Bibliotecario.EsPalindroma(noPalindromo), false);
+        }
+
+        [TestMethod]
+        public void EsMayorQue()
+        {
+            int maximo = 30;
+            string isbn = "9UH76J899789";
+            Assert.AreEqual(Bibliotecario.EsMayorQue(maximo, isbn), true);
+        }
+
+        [TestMethod]
+        public void CalcularFechaMaximaTest()
+        {
+            DateTime fechaEntregaCorrecta =  new DateTime();
+            int DIAS_MAXIMO_DE_ENTREGA = 15;
+            string isbn = "9UH76J899789";
+
+            for (int k = 1; k < DIAS_MAXIMO_DE_ENTREGA; k++){
+                while (fechaEntregaCorrecta.DayOfWeek == DayOfWeek.Sunday) fechaEntregaCorrecta = fechaEntregaCorrecta.AddDays(1);
+                fechaEntregaCorrecta = fechaEntregaCorrecta.AddDays(1);
+            }
+
+            Assert.Equals(Bibliotecario.CalcularFechaEntregaMaxima(isbn),fechaEntregaCorrecta);
+        }
+
+    }
+}
